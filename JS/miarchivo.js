f5c31f82eb92d9e5e0332e2520ca57b8c4f6a9fb
Catalina Riquelme <riquelmecata@gmail.com>
2022-12-07 16:44:09 -0300

    function

--- JS/miarchivo.js
+++ JS/miarchivo.js
@@ -14,22 +14,30 @@ let horas_trabajadas = parseFloat(prompt("Ingrese número de horas trabajadas"))
 console.log(+horas_trabajadas);
 
 while ((nombre_dias != "ESC") && (nombre_dias != null)) {
+
+    // suma de horas trabajadas durante el día
     suma_horas = suma_horas + horas_trabajadas;
     console.log("Suma horas: " +suma_horas+ " horas");
+
+    // valor de la hora trabajada
     console.log("Valor hora: $" +valor_hora);
     pago_dia = horas_trabajadas * valor_hora;
+
+    // pago del día
     console.log("El pago del dia es $ " +pago_dia);
     monto = monto + pago_dia;
     console.log("Sueldo: " +monto);
 
+    // si se pasa de las 45 horas legales semanales hay un break
     if(suma_horas >= max_horas) {
         break;
     }
-
+    
+    // suma de los días trabajados
     suma_dias++;
     console.log("Suma días trabajados: "+suma_dias);
 
-
+    // si se pasa de los 7 días de la semana hay un break
     if (suma_dias >= max_dias) {
         break;
     }
@@ -46,8 +54,15 @@ while ((nombre_dias != "ESC") && (nombre_dias != null)) {
     }
 }
 
-// descuentos
-monto = monto - ((monto * descuentos_legales) / 100);
+
+// descuentos con función
+function quitarIVA(monto) {
+    return monto - ((monto * descuentos_legales) / 100);
+}
+let sueldoLiquido = quitarIVA(monto);
+alert("Recibirás un pago de "+sueldoLiquido.toFixed(0)+ " pesos líquido");
+
+/* monto = monto - ((monto * descuentos_legales) / 100); */
 
 // salida final
-alert("Recibirás un pago de " +monto.toFixed(0)+ " pesos líquido");
+/* alert("Recibirás un pago de " +monto.toFixed(0)+ " pesos líquido"); */
